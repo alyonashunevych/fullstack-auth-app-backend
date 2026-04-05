826f092e082a4002b02421c2e71691f6578f9fd1
Alyona Shunevych <olena17ju06@gmail.com>
2026-04-05 16:25:19 +0300

    Initial commit

--- src/server/utils/db.ts
+++ src/server/utils/db.ts
@@ -3,6 +3,6 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaPg({ connectionString: process.env.DB_URL });
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
 export const db = new PrismaClient({ adapter });
